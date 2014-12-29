a99a96ddc8048500090cf00ce294699d55bdfb4d
Even Holthe <even.holthe@me.com>
2014-12-29 14:35:46 +0100

    Fixed originating IPs

--- server.js
+++ server.js
@@ -4,17 +4,17 @@
  * Coded by Even Holthe for use in a
  * Android project @ HiOA
  */
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var request = require('request');
+ var express = require('express');
+ var app = express();
+ var bodyParser = require('body-parser');
+ var request = require('request');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
+ app.use(bodyParser.urlencoded({ extended: true }));
+ app.use(bodyParser.json());
+ app.use(express.static(__dirname + '/public'));
 
-var port = process.env.PORT || 8080;
-var router = express.Router();
+ var port = process.env.PORT || 8080;
+ var router = express.Router();
 
 // Nobil specific
 nobil_api = 'http://nobil.no/api/server/search.php';
@@ -22,313 +22,313 @@ nobil_api_key = "";
 
 // For every request recieved
 router.use(function (req, res, next) {
-    console.log('Recieved request from ' + req.ip + ' for ' + req.path);
+  console.log('Recieved request from ' + req.get('X-Forwarded-For') + ' for ' + req.path);
 
     // Fetch API-key from headers
     nobil_api_key = req.get('X-Authorization');
 
     if (!nobil_api_key) {
-        console.log("\tClient is missing API key, rejecting");
-        res.json({ error: "API key is missing!" });
+      console.log("\tClient is missing API key, rejecting");
+      res.json({ error: "API key is missing!" });
     } else {
-        baseRequest = request.defaults({
-            headers: {
-                'X-Forwarded-For': req.ip
-            }
-        });
+      baseRequest = request.defaults({
+        headers: {
+          'X-Forwarded-For': req.ip
+        }
+      });
 
-        next();
+      next();
     }
-});
+  });
 
 /* CHARGERS */
 
 // A single charger
 router.get('/chargers/id/:charger_id', function (req, res) {
-    if (!req.params.charger_id) res.status(400).send({ error: "Missing charger id" });
+  if (!req.params.charger_id) res.status(400).send({ error: "Missing charger id" });
 
-    baseRequest.post({
-        url: nobil_api,
-        form: {
-            apikey: nobil_api_key,
-            apiversion: 3,
-            action: "search",
-            type: "id",
-            id: req.params.charger_id
-        }
-    }, function (err, httpResponse, body) {
-        if (err) res.status(406).send(err);
+  baseRequest.post({
+    url: nobil_api,
+    form: {
+      apikey: nobil_api_key,
+      apiversion: 3,
+      action: "search",
+      type: "id",
+      id: req.params.charger_id
+    }
+  }, function (err, httpResponse, body) {
+    if (err) res.status(406).send(err);
 
-        var object = JSON.parse(body);
+    var object = JSON.parse(body);
 
-        if (object.error != undefined) {
-            res.status(400).send(object)
-        } else {
-            res.json(object.chargerstations[0]);
-        }
-    });
+    if (object.error != undefined) {
+      res.status(400).send(object)
+    } else {
+      res.json(object.chargerstations[0]);
+    }
+  });
 });
 
 // Gets chargers for a given place
 router.get('/chargers/place/:place', function (req, res) {
-    if (!req.params.place) res.status(400).send({ error: "No place specified" });
+  if (!req.params.place) res.status(400).send({ error: "No place specified" });
 
-    request.get("https://maps.googleapis.com/maps/api/geocode/json?region=no&address=" + req.params.place, function (error, response, body) {
-        if (error) res.status(504).send({ error: "Problem while asking Google for geocode" });
+  request.get("https://maps.googleapis.com/maps/api/geocode/json?region=no&address=" + req.params.place, function (error, response, body) {
+    if (error) res.status(504).send({ error: "Problem while asking Google for geocode" });
 
-        if (JSON.parse(body).status != "ZERO_RESULTS") {
-            var bounds = JSON.parse(body).results[0].geometry.viewport;
-        } else {
-            console.log("\tGoogle returned no results for the place: " + req.params.place);
+    if (JSON.parse(body).status != "ZERO_RESULTS") {
+      var bounds = JSON.parse(body).results[0].geometry.viewport;
+    } else {
+      console.log("\tGoogle returned no results for the place: " + req.params.place);
+    }
+
+    if (!bounds) {
+      res.status(400).send({ error: "No matches" });
+    } else {
+      var ne = "(" + bounds.northeast.lat + ", " + bounds.northeast.lng + ")";
+      var sw = "(" + bounds.southwest.lat + ", " + bounds.southwest.lng + ")";
+
+      baseRequest.post({
+        url: nobil_api,
+        form: {
+          apikey: nobil_api_key,
+          apiversion: 3,
+          action: "search",
+          type: "rectangle",
+          northeast: ne,
+          southwest: sw,
         }
+      }, function (err, httpResponse, body) {
+        if (err) res.status(406)
+          .send(err);
 
-        if (!bounds) {
-            res.status(400).send({ error: "No matches" });
+        var object = JSON.parse(body);
+
+        if (object.error != undefined) {
+          res.status(400).send(object)
         } else {
-            var ne = "(" + bounds.northeast.lat + ", " + bounds.northeast.lng + ")";
-            var sw = "(" + bounds.southwest.lat + ", " + bounds.southwest.lng + ")";
-
-            baseRequest.post({
-                url: nobil_api,
-                form: {
-                    apikey: nobil_api_key,
-                    apiversion: 3,
-                    action: "search",
-                    type: "rectangle",
-                    northeast: ne,
-                    southwest: sw,
-                }
-            }, function (err, httpResponse, body) {
-                if (err) res.status(406)
-                    .send(err);
-
-                var object = JSON.parse(body);
-
-                if (object.error != undefined) {
-                    res.status(400).send(object)
-                } else {
-                    res.json(object.chargerstations);
-                }
-            });
+          res.json(object.chargerstations);
         }
-    });
+      });
+    }
+  });
 });
 
 // A charger my map references
 router.get('/chargers/map/:northeast/:southwest', function (req, res) {
-    if (!req.params.northeast || !req.params.southwest) res.status(400).send({ error: "Missing northeast and southwest map coordinates!" });
+  if (!req.params.northeast || !req.params.southwest) res.status(400).send({ error: "Missing northeast and southwest map coordinates!" });
 
-    baseRequest.post({
-        url: nobil_api,
-        form: {
-            apikey: nobil_api_key,
-            apiversion: 3,
-            action: "search",
-            type: "rectangle",
-            northeast: req.params.northeast,
-            southwest: req.params.southwest,
-        }
-    }, function (err, httpResponse, body) {
-        if (err) res.status(406).send(err);
+  baseRequest.post({
+    url: nobil_api,
+    form: {
+      apikey: nobil_api_key,
+      apiversion: 3,
+      action: "search",
+      type: "rectangle",
+      northeast: req.params.northeast,
+      southwest: req.params.southwest,
+    }
+  }, function (err, httpResponse, body) {
+    if (err) res.status(406).send(err);
 
-        var object = JSON.parse(body);
+    var object = JSON.parse(body);
 
-        if (object.error != undefined) {
-            res.status(400).send(object)
-        } else {
-            res.json(object.chargerstations);
-        }
+    if (object.error != undefined) {
+      res.status(400).send(object)
+    } else {
+      res.json(object.chargerstations);
+    }
 
-    });
+  });
 });
 
 // A charger near a map reference.
 router.get('/chargers/near/:lat/:lon', function (req, res) {
-    if (!req.params.lat || !req.params.lon) res.status(400).send({ error: "Missing latitude and longitude coordinates!" });
+  if (!req.params.lat || !req.params.lon) res.status(400).send({ error: "Missing latitude and longitude coordinates!" });
 
-    baseRequest.post({
-        url: nobil_api,
-        form: {
-            apikey: nobil_api_key,
-            apiversion: 3,
-            action: "search",
-            type: "near",
-            lat: req.params.lat,
-            long: req.params.lon,
-            distance: (!req.query.distance ? 500 : req.query.distance),
-            limit: (!req.query.limit ? 10 : req.query.limit)
-        }
-    }, function (err, httpResponse, body) {
-        if (err) res.status(406).send(err);
+  baseRequest.post({
+    url: nobil_api,
+    form: {
+      apikey: nobil_api_key,
+      apiversion: 3,
+      action: "search",
+      type: "near",
+      lat: req.params.lat,
+      long: req.params.lon,
+      distance: (!req.query.distance ? 500 : req.query.distance),
+      limit: (!req.query.limit ? 10 : req.query.limit)
+    }
+  }, function (err, httpResponse, body) {
+    if (err) res.status(406).send(err);
 
-        var object = JSON.parse(body);
+    var object = JSON.parse(body);
 
-        if (object.error != undefined) {
-            res.status(400).send(object)
-        } else {
-            res.json(object.chargerstations);
-        }
+    if (object.error != undefined) {
+      res.status(400).send(object)
+    } else {
+      res.json(object.chargerstations);
+    }
 
-    });
+  });
 });
 
 /* STATS */
 
 // Get stats for all counties
 router.get('/stats/:country', function (req, res) {
-    if (req.params.country == "NOR" || req.params.country == "SWE" || req.params.country == "DAN") {
-        baseRequest.post({
-            url: nobil_api,
-            form: {
-                apikey: nobil_api_key,
-                apiversion: 3,
-                action: "search",
-                type: "stats_TotalsAllCounties",
-                countrycode: req.params.country
-            }
-        }, function (err, httpResponse, body) {
-            if (err) res.status(406).send(err);
+  if (req.params.country == "NOR" || req.params.country == "SWE" || req.params.country == "DAN") {
+    baseRequest.post({
+      url: nobil_api,
+      form: {
+        apikey: nobil_api_key,
+        apiversion: 3,
+        action: "search",
+        type: "stats_TotalsAllCounties",
+        countrycode: req.params.country
+      }
+    }, function (err, httpResponse, body) {
+      if (err) res.status(406).send(err);
 
-            var object = JSON.parse(body);
+      var object = JSON.parse(body);
 
-            if (object.error != undefined) {
-                res.status(400).send(object)
-            } else {
-                res.json(object.chargerstations);
-            }
-        });
-    } else {
-        res.status(400).send({ error: "Country code must be NOR, SWE or DAN" });
-    }
+      if (object.error != undefined) {
+        res.status(400).send(object)
+      } else {
+        res.json(object.chargerstations);
+      }
+    });
+  } else {
+    res.status(400).send({ error: "Country code must be NOR, SWE or DAN" });
+  }
 });
 
 // Get stats for a single county
 router.get('/stats/:country/county/:county_id', function (req, res) {
-    if (req.params.country == "NOR" || req.params.country == "SWE" || req.params.country == "DAN") {
+  if (req.params.country == "NOR" || req.params.country == "SWE" || req.params.country == "DAN") {
         // If empty county id
         if (!req.params.county_id) res.status(400).send({ error: "No county code specified" });
 
         baseRequest.post({
-            url: nobil_api,
-            form: {
-                apikey: nobil_api_key,
-                apiversion: 3,
-                action: "search",
-                type: "stats_TotalsByCountyId",
-                countrycode: req.params.country,
-                id: req.params.county_id
-            }
+          url: nobil_api,
+          form: {
+            apikey: nobil_api_key,
+            apiversion: 3,
+            action: "search",
+            type: "stats_TotalsByCountyId",
+            countrycode: req.params.country,
+            id: req.params.county_id
+          }
         }, function (err, httpResponse, body) {
-            if (err) res.status(406).send(err);
+          if (err) res.status(406).send(err);
 
-            var object = JSON.parse(body);
+          var object = JSON.parse(body);
 
-            if (object.error != undefined) {
-                res.status(400).send(object)
-            } else {
-                res.json(object.chargerstations[0]);
-            }
+          if (object.error != undefined) {
+            res.status(400).send(object)
+          } else {
+            res.json(object.chargerstations[0]);
+          }
         });
-    } else {
+      } else {
         res.status(400).send({ error: "Country code must be NOR, SWE or DAN" });
-    }
-});
+      }
+    });
 
 // Get stats for a single county with municipalities
 router.get('/stats/:country/county/:county_id/municipalities', function (req, res) {
-    if (req.params.country == "NOR" || req.params.country == "SWE" || req.params.country == "DAN") {
+  if (req.params.country == "NOR" || req.params.country == "SWE" || req.params.country == "DAN") {
         // If empty county id
         if (!req.params.county_id) res.status(400).send({ error: "No county code specified" });
 
         baseRequest.post({
-            url: nobil_api,
-            form: {
-                apikey: nobil_api_key,
-                apiversion: 3,
-                action: "search",
-                type: "stats_DetailedTotalsByCountyId",
-                countrycode: req.params.country,
-                id: req.params.county_id
-            }
+          url: nobil_api,
+          form: {
+            apikey: nobil_api_key,
+            apiversion: 3,
+            action: "search",
+            type: "stats_DetailedTotalsByCountyId",
+            countrycode: req.params.country,
+            id: req.params.county_id
+          }
         }, function (err, httpResponse, body) {
-            if (err) res.status(406).send(err);
+          if (err) res.status(406).send(err);
 
-            var object = JSON.parse(body);
+          var object = JSON.parse(body);
 
-            if (object.error != undefined) {
-                res.status(400).send(object)
-            } else {
-                res.json(object.chargerstations);
-            }
+          if (object.error != undefined) {
+            res.status(400).send(object)
+          } else {
+            res.json(object.chargerstations);
+          }
         });
-    } else {
+      } else {
         res.status(400).send({error: "Country code must be NOR, SWE or DAN"});
-    }
-});
+      }
+    });
 
 // Get stats for a single county with municipalities
 router.get('/stats/:country/municipalities/:municipality', function (req, res) {
-    if (req.params.country == "NOR" || req.params.country == "SWE" || req.params.country == "DAN") {
+  if (req.params.country == "NOR" || req.params.country == "SWE" || req.params.country == "DAN") {
         // If empty municipality id
         if (!req.params.municipality) res.status(400).send({ error: "No municipality id specified" });
 
         baseRequest.post({
-            url: nobil_api,
-            form: {
-                apikey: nobil_api_key,
-                apiversion: 3,
-                action: "search",
-                type: "stats_TotalsByMunicipalId",
-                countrycode: req.params.country,
-                id: req.params.municipality
-            }
+          url: nobil_api,
+          form: {
+            apikey: nobil_api_key,
+            apiversion: 3,
+            action: "search",
+            type: "stats_TotalsByMunicipalId",
+            countrycode: req.params.country,
+            id: req.params.municipality
+          }
         }, function (err, httpResponse, body) {
-            if (err) res.status(406).send(err);
+          if (err) res.status(406).send(err);
 
-            var object = JSON.parse(body);
+          var object = JSON.parse(body);
 
-            if (object.error != undefined) {
-                res.status(400).send(object)
-            } else {
-                res.json(object.chargerstations[0]);
-            }
+          if (object.error != undefined) {
+            res.status(400).send(object)
+          } else {
+            res.json(object.chargerstations[0]);
+          }
         });
-    } else {
+      } else {
         res.status(400).send({ error: "Country code must be NOR, SWE or DAN" });
-    }
-});
+      }
+    });
 
 // Get detailed stats for a single county with municipalities
 router.get('/stats/:country/municipalities/:municipality/detail', function (req, res) {
-    if (req.params.country == "NOR" || req.params.country == "SWE" || req.params.country == "DAN") {
+  if (req.params.country == "NOR" || req.params.country == "SWE" || req.params.country == "DAN") {
         // If empty municipality id
         if (!req.params.municipality) res.status(400).send({ error: "No municipality id specified" });
 
         baseRequest.post({
-            url: nobil_api,
-            form: {
-                apikey: nobil_api_key,
-                apiversion: 3,
-                action: "search",
-                type: "stats_DetailTotalsByMunicipalId",
-                countrycode: req.params.country,
-                id: req.params.municipality
-            }
+          url: nobil_api,
+          form: {
+            apikey: nobil_api_key,
+            apiversion: 3,
+            action: "search",
+            type: "stats_DetailTotalsByMunicipalId",
+            countrycode: req.params.country,
+            id: req.params.municipality
+          }
         }, function (err, httpResponse, body) {
-            if (err) res.status(406).send(err);
+          if (err) res.status(406).send(err);
 
-            var object = JSON.parse(body);
+          var object = JSON.parse(body);
 
-            if (object.error != undefined) {
-                res.status(400).send(object)
-            } else {
-                res.json(object.chargerstations);
-            }
+          if (object.error != undefined) {
+            res.status(400).send(object)
+          } else {
+            res.json(object.chargerstations);
+          }
         });
-    } else {
+      } else {
         res.status(400).send({ error: "Country code must be NOR, SWE or DAN" });
-    }
-});
+      }
+    });
 
 
 // Path for API calls (/api)
--- test/test.js
+++ test/test.js
@@ -17,10 +17,10 @@ describe('Nobil API Proxy tests', function() {
       .set(auth)
       .end(function(err, res) {
         if (err) throw err;
-          should(res.status).be.exactly(200);
-          should(res.body.csmd).have.property('id');
-          done();
-        });
+        should(res.status).be.exactly(200);
+        should(res.body.csmd).have.property('id');
+        done();
+      });
     });
 
     it('should provide an error searching for an invalid charger', function(done) {
@@ -29,10 +29,10 @@ describe('Nobil API Proxy tests', function() {
       .set(auth)
       .end(function(err, res) {
         if (err) throw err;
-          should(res.status).be.exactly(400);
-          should(res.body).have.property('error');
-          done();
-        });
+        should(res.status).be.exactly(400);
+        should(res.body).have.property('error');
+        done();
+      });
     });
 
   });
@@ -45,26 +45,88 @@ describe('Nobil API Proxy tests', function() {
       .set(auth)
       .end(function(err, res) {
         if (err) throw err;
-          should(res.status).be.exactly(200);
-          should(res.body).not.have.property('error');
-          should(res.body[0].csmd).have.property('id');
-          done();
-        });
+        should(res.status).be.exactly(200);
+        should(res.body).not.have.property('error');
+        should(res.body[0].csmd).have.property('id');
+        done();
+      });
     });
 
-    it('should not find chargers when no place is specified', function(done) {
+    it('should not find chargers when place is not specified', function(done) {
       request(url)
       .get('/chargers/place/0')
       .set(auth)
       .end(function(err, res) {
         if (err) throw err;
-          should(res.status).be.exactly(400);
-          should(res.body).have.property('error');
-          done();
-        });
+        should(res.status).be.exactly(400);
+        should(res.body).have.property('error');
+        done();
+      });
+    });
+
+    it('should not find chargers in Hell', function(done) {
+      request(url)
+      .get('/chargers/place/Hell')
+      .set(auth)
+      .end(function(err, res) {
+        if (err) throw err;
+        should(res.status).be.exactly(400);
+        should(res.body).have.property('error');
+        done();
+      });
     });
 
 
+    it('should find chargers by map references', function(done) {
+      this.timeout(6000);
+
+      request(url)
+      .get('/chargers/map/(59.943921193288915,10.826683044433594)/(59.883683240905256,10.650901794433594)')
+      .set(auth)
+      .end(function(err, res) {
+        if (err) throw err;
+        should(res.status).be.exactly(200);
+        should(res.body).not.have.property('error');
+        done();
+      });
+    });
+
+
+    it('should not find chargers by invalid map references', function(done) {
+      request(url)
+      .get('/chargers/map/(foo,bar)/(baz,123)')
+      .set(auth)
+      .end(function(err, res) {
+        if (err) throw err;
+        should(res.status).be.exactly(400);
+        should(res.body).have.property('error');
+        done();
+      });
+    });
+
+    it('should find chargers near coordinates', function(done) {
+      request(url)
+      .get('/chargers/near/59.91673/10.74782?distance=500&limit=10')
+      .set(auth)
+      .end(function(err, res) {
+        if (err) throw err;
+        should(res.status).be.exactly(200);
+        should(res.body).not.have.property('error');
+        done();
+      });
+    });
+
+    it('should not find chargers near invalid coordinates', function(done) {
+      request(url)
+      .get('/chargers/near/2/3.14?distance=500&limit=10')
+      .set(auth)
+      .end(function(err, res) {
+        if (err) throw err;
+        should(res.status).be.exactly(400);
+        should(res.body).have.property('error');
+        done();
+      });
+    });
 
   });
 
